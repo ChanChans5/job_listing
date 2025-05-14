d094b4701f1cfbd664f12907a16f2430796f8fb4
unknown <chansalonoy96@gmail.com>
2025-05-15 01:32:53 +0800

    commit

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
     <title>Document</title>
 </head>
 
